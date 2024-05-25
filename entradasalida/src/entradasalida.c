a0b7d5298f8a06c8373e878b091f62e35e4387fc
Bruno <brunoliserre@gmail.com>
2024-05-25 01:49:28 -0300

    Agregado IO_GEN_SLEEP, serializacion de estructuras de E/S

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -4,13 +4,25 @@ int main(void){
     //Inicializo Estructuras E/S
     inicializarEstructurasEntradaSalida();
 
+    // Crear lista vacía
+    lista = list_create();
+
     //Conexion E/S - Kernel
-   // fd_es = crear_conexion(ipKernel, puertoKernel);
+    fd_es = crear_conexion(ipKernel, puertoKernel);
+    if (fd_es == -1) {
+        log_error(loggerEntradaSalida, "Error al conectar con el Kernel.");
+        terminar_programa(loggerEntradaSalida, configEntradaSalida);
+        return EXIT_FAILURE;
+    }
 
     //Handshake con kernel
-   // handshakeCliente(fd_es, loggerEntradaSalida);
+    //handshakeCliente(fd_es, loggerEntradaSalida);
 
-    
+    //Cargo Interfaz en lista
+    agregar_paquete_gen(lista, nombre, con, disp, loggerEntradaSalida);
+
+    //Serializacion
+    //serializar_lista(lista, fd_es, loggerEntradaSalida);
 
     //Conexion E/S - Memoria
     fd_esMem = crear_conexion(ipMemoria, puertoMemoria);
@@ -22,7 +34,7 @@ int main(void){
     liberar_conexion(fd_esMem);
 
     //Terminar programa
-    terminar_programa(loggerEntradaSalida, configEntradaSalida);
+    //terminar_programa(loggerEntradaSalida, configEntradaSalida);
 }
 
 
@@ -33,32 +45,109 @@ void inicializarEstructurasEntradaSalida(void){
     ipKernel = config_get_string_value(configEntradaSalida, "IP_KERNEL");
     puertoMemoria = config_get_string_value(configEntradaSalida, "PUERTO_MEMORIA");
     ipMemoria = config_get_string_value(configEntradaSalida, "IP_MEMORIA");
+    tiempoTrabajo = config_get_int_value(configEntradaSalida, "TIEMPO_UNIDAD_TRABAJO");
 }
 
-/*void handshakeEntradaSalida(int fd_es, t_log* loggerEntradaSalida) {
-    size_t bytes;
-    int32_t handshake = 1;
-    int32_t result;
 
-    bytes = send(fd_es, &handshake, sizeof(int32_t), 0);
-    if (bytes < 0) {
-        log_error(loggerEntradaSalida, "Error al enviar el mensaje de handshake");
+//Instruccion IO_GEN_SLEEP 
+//Espera segun la cantidad de unidades de trabajo que se le pase de kernel
+void io_gen_sleep(char* tipoInt, int ut, t_log* logger) {
+    if (strcmp(tipoInt, "GENERICA") != 0)
+    {
+        log_error(loggerEntradaSalida, "No es del tipo generica");
         return;
     }
-
-    bytes = recv(fd_es, &result, sizeof(int32_t), MSG_WAITALL);
-    if (bytes < 0) {
-        log_error(loggerEntradaSalida, "Error al recibir la respuesta de handshake");
-        return;
-    }
-
-    if (result == 0) {
-        log_info(loggerEntradaSalida, "Handshake completado con éxito");
-    } else {
-        log_error(loggerEntradaSalida, "Error en el handshake, código de error: %d", result);
-    }
+    
+    // Calcular el tiempo de espera
+    int total_time = ut * tiempoTrabajo;
+    usleep(total_time * 1000); // usleep opera en microsegundos
+    log_info(loggerEntradaSalida, "Operacion: IO_GEN_SLEEP completado");
 }
-*/
+
+
+// Serializacion
+// Interfaz Genérica 
+// Lleno la lista
+void agregar_paquete_gen(t_list* lista, char* nombre, uint8_t con, uint8_t disp, t_log* logger) {
+    //Hardcodeo prueba
+    nombre = "NombreEjemplo";
+
+    t_paquete_gen* paquete_gen = malloc(sizeof(t_paquete_gen));
+    paquete_gen->nombre = strdup(nombre);
+    paquete_gen->nombre_length = strlen(paquete_gen->nombre);
+    paquete_gen->con = con;
+    paquete_gen->disp = disp;
+    strcpy(paquete_gen->tipoInterfaz, "GENERIC");
+    strcpy(paquete_gen->opAdmitidas, "IO_GEN_SLEEP");
+    list_add(lista, paquete_gen);
+    log_info(logger, "Lista cargada con interfaz");
+}
+
+//Serializo
+void serializar_lista(t_list* lista, int socket_cliente, t_log* logger){
+    t_buffer* buffer = malloc(sizeof(t_buffer));
+
+    // Obtengo el primer paquete de la lista - Generico
+    t_paquete_gen* primer_paquete = (t_paquete_gen*)(lista->head->data);
+
+
+    // Calculo el tamaño del buffer
+    buffer->size = sizeof(uint32_t)                     // nombre_length
+                    + sizeof(uint8_t) *  4              // con, disp, tipoInterfaz, opAdmitidas
+                    + primer_paquete->nombre_length;    // longitud string
+
+    buffer->offset = 0;
+    buffer->stream = malloc(buffer->size);
+
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->con, sizeof(uint8_t));
+    buffer->offset += sizeof(u_int8_t);
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->disp, sizeof(uint8_t));
+    buffer->offset += sizeof(u_int8_t);
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->tipoInterfaz, sizeof(uint8_t));
+    buffer->offset += sizeof(u_int8_t);
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->opAdmitidas, sizeof(uint8_t));
+    buffer->offset += sizeof(u_int8_t);
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->nombre_length, sizeof(uint32_t));
+    buffer->offset += sizeof(u_int32_t);
+    memcpy(buffer->stream + buffer->offset, &primer_paquete->nombre, primer_paquete->nombre_length);
+
+    buffer->stream = buffer->stream;
+
+    free(primer_paquete->nombre);
+
+    //Lleno el paquete con el buffer
+    t_paquete* paquete = malloc(sizeof(t_paquete));
+
+    paquete->codigo_operacion = GENERICO;
+    paquete->buffer = buffer;
+
+    //Armo el stream a enviar
+    void* a_enviar = malloc(buffer->size + sizeof(uint8_t) + sizeof(uint32_t));
+    int offset = 0;
+
+    memcpy(a_enviar + offset, &(paquete->codigo_operacion), sizeof(uint8_t));
+    offset += sizeof(uint8_t);
+    memcpy(a_enviar + offset, &(paquete->buffer->size), sizeof(uint32_t));
+    offset += sizeof(uint32_t);
+    memcpy(a_enviar + offset, &(paquete->buffer->stream), paquete->buffer->size);
+
+    //Envio
+    send(socket_cliente, a_enviar, buffer->size + sizeof(uint8_t) + sizeof(uint32_t),0);
+    log_info(logger, "Interfaz enviada");
+
+    //Liberar memoria
+    free(a_enviar);
+    free(paquete->buffer->stream);
+    free(paquete->buffer);
+    free(paquete);
+}
+
+//STDIN
+
+//STDOUT
+
+//DIALFS
+
 
 /*void terminar_programa(t_log* logger, t_config* config)
 {
--- entradasalida/src/entradasalida.h
+++ entradasalida/src/entradasalida.h
@@ -1,5 +1,6 @@
 #ifndef ENTRADASALIDA_H_
 #define ENTRADASALIDA_H_
+#define GENERICO 1
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,28 +11,87 @@
 #include<commons/string.h>
 #include<commons/config.h>
 
-
 //Utils
 #include <utils/clienteUtils/clienteUtils.h>
 #include <utils/utilsGenerales.h>
 
+//Estructuras
+//Interfaz Generica
+typedef struct {
+    char* nombre;
+    uint32_t nombre_length;
+    uint8_t con;            // Conexión (0 para no conectado, 1 para conectado)
+    uint8_t disp;           // Disponibilidad (0 para no disponible, 1 para disponible)
+    char tipoInterfaz[8];   // "GENERICA"
+    char opAdmitidas[11];   // "IO_GEN_SLEEP"
+} t_paquete_gen;
+
+//Interfaz STDIN
+typedef struct {
+    char* nombre;
+    uint32_t nombre_lenght;
+    uint8_t con;            // Conexión (0 para no conectado, 1 para conectado)
+    uint8_t disp;           // Disponibilidad (0 para no disponible, 1 para disponible)
+    char tipoInterfaz[8];   // "STDIN"
+    char opAdmitidas[11];   // "IO_STDIN_READ"
+} t_paquete_stdin;
+
+//Interfaz STDOUT
+typedef struct {
+    char* nombre;
+    uint32_t nombre_lenght;
+    uint8_t con;            // Conexión (0 para no conectado, 1 para conectado)
+    uint8_t disp;           // Disponibilidad (0 para no disponible, 1 para disponible)
+    char tipoInterfaz[8];   // "STDOUT"
+    char opAdmitidas[11];   // "IO_STDOUT_WRITE"
+} t_paquete_stdout;
+
+//Interfaz DIALFS 
+
+/*//Buffer
+typedef struct {
+    uint32_t size; // Tamaño payload
+    uint32_t offset; // Desplazamiento dentro del playload
+    void* stream; // Payload
+} t_buffer;
+
+//Lista
+typedef struct {
+    void** items;  
+    size_t size;   
+} t_list;
+
+//Protocolo
+typedef struct {
+    uint8_t codigo_operacion;
+    t_buffer* buffer;
+} t_paquete;*/
+
+
+
 //Variables Globales
 int fd_es;
 int fd_esMem;
+int tiempoTrabajo;
+uint8_t con;
+uint8_t disp;
 char* ipKernel;
 char* puertoKernel;
 char* puertoMemoria;
 char* ipMemoria;
 char* valor;
+char* nombre;
+t_list* lista;
 t_log* loggerEntradaSalida;
 t_config*configEntradaSalida;
 
+void agregar_paquete_gen(t_list*, char*, uint8_t, uint8_t, t_log*);
+void serializar_lista(t_list*, int socket_cliente, t_log*);
 void leer_consola(t_log*);
 void paquete(int);
 void inicializarEstructurasEntradaSalida(void);
+void io_gen_sleep(char*, int, t_log*);
 
-//void handshakeEntradaSalida(int, t_log*);
 //void terminar_programa(t_log*, t_config*);
-
 
 #endif /* ENTRADASALIDA_H_ */
